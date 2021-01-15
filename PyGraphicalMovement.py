aa636761f75170ef94db98018f2f0a75b26aa6a6
Zephyy <os06112001@gmail.com>
2021-01-16 00:08:36 +1000

    Некоторые улучшения

--- PyGraphicalMovement.py
+++ PyGraphicalMovement.py
@@ -4,27 +4,59 @@
 """
 import tkinter as tk
 arr = []
+prev_bt = 0
 
 
-def gen_perm_num(num):
+def gen_perm_num(num: int) -> list:
+    """
+    Функция проверки может ли робот проехать в ячейку матрицы
+
+    Проверка нужна так как робот не может двигаться по диагонали
+    в данной реализации
+    """
+    narr = []
     if num == 5:
-        narr = [x for x in range(1, 10) if x != 5]
+        narr = [2, 4, 6, 8]
     elif num in (1, 3, 7, 9):
         x = num - 1 if not num % 3 else num + 1
         narr.append(x)
         y = num + 3 if num < 5 else num - 3
         narr.append(y)
+    elif num in (2, 8):
+        narr = [num - 1, 5, num + 1]
+    elif num in (4, 6):
+        narr = [num - 3, 5, num + 3]
+    else:  # обработка ошибки
+        return
 
     return narr
 
 
-def check_way(num: int, arr: list) -> list:
+def check_way(num: int, arr: list, bt: tk.Button) -> None:
+    """
+    Функция на основе информации полученной с gen_perm_num
+    добавляет номер нажатой кнопки, или нет.
+    """
+    global prev_bt
+    prev_bt = bt
+    
+    print(arr)
+    print(prev_bt)
+    try:
+        prev_bt.config(bg="grey")
+    except Exception:
+        pass
+    
     if len(arr) != 0:
         if 0 < num < 10 and arr[0] != num:
             carr = gen_perm_num(arr[len(arr) - 1])
             if num in carr:
                 arr.append(num)
+                bt.config(bg="blue")
+                bt_prev = bt
             else:
+                bt.config(bg="red")
+                bt_prev = bt
                 return
     else:
         arr.append(num)
@@ -33,22 +65,19 @@ def check_way(num: int, arr: list) -> list:
 def Draw_button() -> list:
     # Функция создания кнопок в графическом интерфейсе
     x, y = 10, 0
-    barr = []
     for i in range(9):
-        barr.append(tk.Button(heigh="5", width="5",
-                              command=lambda i=i+1, arr=arr: arr.append(i)))
-        print(i)
-        barr[i].grid(column=x, row=y)
+        bt = tk.Button(heigh="5", width="5")
+        bt.config(command=lambda i=i+1, arr=arr, bt=bt: check_way(i, arr, bt))
+        bt.grid(column=x, row=y)
         y += 8 if x == 30 else 0
         x = 10 if x == 30 else x + 10
-        print(x, y)
-    return barr
+
 
 
 win = tk.Tk()
 win.title("PyFinchMovement")
 win.geometry("205x300")
 # tabs = Draw_tabs(win)
-buttons = Draw_button()
+Draw_button()
 win.mainloop()
 print(arr)
