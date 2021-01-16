fba66a1b92b4c5a8753d3b408b3a61dad77209b1
Zephyy <os06112001@gmail.com>
2021-01-16 22:40:22 +1000

    Полностью законченный графический интерфейс

--- PyGraphicalMovement.py
+++ PyGraphicalMovement.py
@@ -3,9 +3,17 @@
 для последующего движения Finch robot по заданным кнопкам
 """
 import tkinter as tk
-arr = []
-prev_bt = 0
+from tkinter import messagebox
+import finch as fi
 
+arr = [5]
+
+
+def Start_finch():
+    try:
+        finch = fi.Finch()
+    except Exception:
+        messagebox.showerror("Ошибка", "Не полчилось подключиться к Finch robot")
 
 def gen_perm_num(num: int) -> list:
     """
@@ -26,58 +34,53 @@ def gen_perm_num(num: int) -> list:
         narr = [num - 1, 5, num + 1]
     elif num in (4, 6):
         narr = [num - 3, 5, num + 3]
-    else:  # обработка ошибки
-        return
 
     return narr
 
 
-def check_way(num: int, arr: list, bt: tk.Button) -> None:
+def check_way(num: int, arr: list, text: tk.Label) -> None:
     """
     Функция на основе информации полученной с gen_perm_num
     добавляет номер нажатой кнопки, или нет.
     """
-    global prev_bt
-    prev_bt = bt
-    
-    print(arr)
-    print(prev_bt)
-    try:
-        prev_bt.config(bg="grey")
-    except Exception:
-        pass
-    
-    if len(arr) != 0:
-        if 0 < num < 10 and arr[0] != num:
-            carr = gen_perm_num(arr[len(arr) - 1])
-            if num in carr:
-                arr.append(num)
-                bt.config(bg="blue")
-                bt_prev = bt
-            else:
-                bt.config(bg="red")
-                bt_prev = bt
-                return
-    else:
-        arr.append(num)
+
+    if 0 < num < 10:
+        carr = gen_perm_num(arr[len(arr) - 1])
+
+        if num in carr:
+            marr = gen_perm_num(num)
+            mess = "Вы находитесь в ячейке [{}]\nВозможный путь {}".format(
+                num, marr)
+            arr.append(num)
+            text.config(text=mess)
+        else:
+            return
+    print(arr)  # Debugg
 
 
-def Draw_button() -> list:
-    # Функция создания кнопок в графическом интерфейсе
-    x, y = 10, 0
+def Draw_elements(win: tk.Tk) -> None:
+    start_msg = "Вы находитесь в ячейке [5]\nВозможный путь [2 4 6 8]"
+    txt = tk.Label(win, text=start_msg, width="27", relief=tk.GROOVE, bg='white')
+    txt.place(relx=0.0, rely=0.9)
+
+    x, y = 1, 0
     for i in range(9):
-        bt = tk.Button(heigh="5", width="5")
-        bt.config(command=lambda i=i+1, arr=arr, bt=bt: check_way(i, arr, bt))
-        bt.grid(column=x, row=y)
-        y += 8 if x == 30 else 0
-        x = 10 if x == 30 else x + 10
+        tk.Button(win, heigh="5", width="5", bg='white', text=i+1,
+                  relief=tk.GROOVE,
+                  command=lambda i=i+1:
+                  check_way(i, arr, txt)).grid(column=x, row=y)
+        y += 1 if x == 3 else 0
+        x = 1 if x == 3 else x + 1
+
+    tk.Button(win, width="23", text='Start', bg='deep sky blue',
+              relief=tk.GROOVE,
+              command=Start_finch).place(x=0, y=240)  # Command = start_finch
 
 
-
-win = tk.Tk()
-win.title("PyFinchMovement")
-win.geometry("205x300")
-# tabs = Draw_tabs(win)
-Draw_button()
-win.mainloop()
+root = tk.Tk()
+root.title("PyFinchMovement")
+root.geometry("190x300")
+Draw_elements(root)
+root.resizable(0, 0)
+root.mainloop()
 print(arr)
