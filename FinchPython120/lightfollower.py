ca6f733f562382f38d532c9d6431f34680d58308
OutOfFlex <vipxomyachokvip@mail.ru>
2021-03-11 17:54:30 +1000

    test version 0.2

--- FinchPython120/lightfollower.py
+++ FinchPython120/lightfollower.py
@@ -5,10 +5,10 @@ from finch import Finch
 finch = Finch()
 
 
-# light – returns the left and right light sensor readings, values range from 0.0 (dark) to 1.0 (bright)
-lls, rls = finch.light()
+
 while True:
     try:
+        # light – returns the left and right light sensor readings, values range from 0.0 (dark) to 1.0 (bright)
         lls, rls = finch.light()
         finch.wheels(lls, rls)
 
