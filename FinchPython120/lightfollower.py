bc488a2ffc2810078eebc6bcbee763160182e9bf
Evgenii <vretmanit@gmail.com>
2021-03-11 19:07:20 +1000

    Рабочее слежование за светом

--- FinchPython120/lightfollower.py
+++ FinchPython120/lightfollower.py
@@ -10,7 +10,7 @@ while True:
     try:
         # light – returns the left and right light sensor readings, values range from 0.0 (dark) to 1.0 (bright)
         lls, rls = finch.light()
-        finch.wheels(lls, rls)
+        finch.wheels(rls, lls)
 
     except Exception:
         print('Error')
